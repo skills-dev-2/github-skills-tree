348a3bbe576a21cdacf0093bfa69ad8f2beed31c
Christopher W. Blake <chriswblake@github.com>
2025-08-22 13:59:02 +0000

    Edited Spark

---
+++ push-to-prod.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# How to run this script:
+# bash push-to-prod.sh
+
+# Load default settings and token.  from .env file
+# REPO
+# GH_TOKEN
+source prod.env
+
+# If Repo is not set, exit.
+if [ -z "$REPO" ]; then
+  echo "REPO is not set. Exiting."
+  exit 1
+fi
+
+# If GH_TOKEN is not set, exit.
+if [ -z "$GH_TOKEN" ]; then
+  echo "GH_TOKEN is not set. Exiting."
+  exit 1
+fi
+
+# Configure remote
+REMOTE_NAME=prod-target
+URL="https://$GH_TOKEN@github.com/$REPO.git"
+git remote | grep $REMOTE_NAME || git remote add $REMOTE_NAME $URL
+git remote set-url $REMOTE_NAME $URL
+
+# Push to remote
+git push -u $REMOTE_NAME main
+
+# Remote the remote since the token is hardcoded in the URL
+git remote remove $REMOTE_NAME
